be92883dfb1db4b8dd4cb3fd835649d7ae5f0a70
nvh0412 <nvh0412@gmail.com>
2013-10-23 16:09:49 +0000

    hoa commit lan 3

--- Phongcoho/src/hoa/Hoa.java
+++ Phongcoho/src/hoa/Hoa.java
@@ -5,6 +5,7 @@ public class Hoa {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hoa package");
+		System.out.println("hoa package");
 	}
 
 }
