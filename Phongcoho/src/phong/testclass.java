9c570eed76b8833d5400d50e37d5012a443eea70
hongphongcntt <hongphongcntt@gmail.com>
2013-10-23 15:48:30 +0000

    cho hoa oi tao dang test ne

---
+++ Phongcoho/src/phong/testclass.java
@@ -0,0 +1,13 @@
+package phong;
+
+public class testclass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("cho hoa");
+	}
+
+}
